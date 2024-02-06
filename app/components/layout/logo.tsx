25e71a42305e2f1221de28598f5f775193be3d62
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-06 15:57:13 -0500

    Changes webpage logo image to be sports related

--- app/components/layout/logo.tsx
+++ app/components/layout/logo.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import Link from "next/link";
-
 import localFont from "next/font/local";
+
+import { FaVolleyballBall } from "react-icons/fa";
 
 import { cn } from "@/lib/utils";
 
@@ -13,7 +13,7 @@ export const Logo = () => {
   return (
     <Link href={"/"}>
       <div className="hover:opacity-75 transition hidden md:flex items-center justify-center gap-x-2">
-        <Image src={"/logo.svg"} alt="Logo" height={30} width={30} />
+        <FaVolleyballBall size={24} />
         <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           Court Booking
         </p>
