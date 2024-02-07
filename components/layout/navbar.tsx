92d970a3d24cf941b7738d740e357362f2e64587
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-07 15:46:09 -0500

    Sets up navbar to be fixed ontop

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -4,18 +4,20 @@ import { Logo } from "./logo";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between items-center bg-neutral-300 py-4 px-8">
-      <Logo />
-      <div className="text-3xl text-center">Book a court today!</div>
-      <div className="mr-4">
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
+    <nav className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+      <div className="flex flex-row justify-between items-center bg-neutral-300 py-4 px-8">
+        <Logo />
+        <div className="text-3xl text-center">Book a court today!</div>
+        <div className="mr-4">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
