672c693f94bbd80bf9b633cd5ead5e3aa0d1a236
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-10 21:59:22 -0500

    Fixes styling to remove white space

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -4,8 +4,8 @@ import { Logo } from "./logo";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
-      <div className="flex flex-row justify-between items-center bg-neutral-300 py-4 px-8">
+    <nav className="fixed top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <div className="flex flex-row w-full justify-between items-center bg-neutral-300 py-4 px-8">
         <Logo />
         <div className="text-3xl text-center">Book a court today!</div>
         <div className="mr-4">
