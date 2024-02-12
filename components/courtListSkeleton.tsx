b8a07370fcacbae3cb764da7ea14d232dffe8359
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-12 15:21:04 -0500

    Moves courtListSkeleton to new component to allow courtList to be client side component

---
+++ components/courtListSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Skeleton } from "./ui/skeleton";
+
+const CourtListSkeleton = () => {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="w-full h-80" />
+      <Skeleton className="w-full h-80" />
+      <Skeleton className="w-full h-80" />
+      <Skeleton className="w-full h-80" />
+      <Skeleton className="w-full h-80" />
+      <Skeleton className="w-full h-80" />
+      <Skeleton className="w-full h-80" />
+      <Skeleton className="w-full h-80" />
+    </div>
+  );
+};
+
+export default CourtListSkeleton;
