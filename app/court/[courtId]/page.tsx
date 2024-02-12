244fe8e346bcda21dcd9dbf41a360759d44346a9
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-11 23:02:51 -0500

    Fixes missing page error and shows message if court data failed to fetch

--- app/court/[courtId]/page.tsx
+++ app/court/[courtId]/page.tsx
@@ -15,4 +15,16 @@ const CourtIdPage = async ({ params }: CourtIdPageProps) => {
       reservations: true,
     },
   });
+
+  if (!courtData) {
+    return (
+      <div className="flex items-center justify-center w-full h-screen">
+        <p>Could not fetch court data</p>
+      </div>
+    );
+  }
+
+  return <div>{courtData.title}</div>;
 };
+
+export default CourtIdPage;
