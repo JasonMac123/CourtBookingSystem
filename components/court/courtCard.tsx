4d40a12f163627cb3ce1bd39296faf07d0034b84
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-13 15:50:34 -0500

    Styles court card with apprioate styling

--- components/court/courtCard.tsx
+++ components/court/courtCard.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { FaLocationDot } from "react-icons/fa6";
+
 import { CourtWithSports } from "@/types";
 
 interface CourtCardProps {
@@ -13,9 +15,9 @@ export const CourtCard = ({ data }: CourtCardProps) => {
   return (
     <Link
       href={`/court/${data.id}`}
-      className="flex w-full bg-neutral-300 h-64 rounded-lg"
+      className="flex w-full bg-neutral-300 h-64 rounded-lg hover:scale-105 transition-all"
     >
-      <div className="w-3/5 h-full relative rounded-l-lg overflow-auto">
+      <div className="w-[55%] h-full relative rounded-l-lg overflow-auto">
         <Image
           src={data.imageId}
           alt="picture of court"
@@ -23,16 +25,21 @@ export const CourtCard = ({ data }: CourtCardProps) => {
           className="object-fit"
         />
       </div>
-      <div className="flex flex-col mx-4 mt-2">
-        <h2 className="text-3xl">{data.title}</h2>
-        <h4>
-          Sports Allowed : {data.sports.map((item) => item.title).toString()}
-        </h4>
-        <div className="flex-row">
-          <div>
-            <h4>{data.address}</h4>
-            <h4>{data.region}</h4>
+      <div className="flex flex-col justify-between w-[45%] mx-4 mt-1">
+        <div className="space-y-1">
+          <h2 className="text-3xl">{data.title}</h2>
+          <h3>{data.description.substring(0, 50)}...</h3>
+          <h4>Supports {data.sports.map((item) => item.title).toString()}</h4>
+        </div>
+        <div>
+          <div className="flex items-center gap-2">
+            <FaLocationDot size={30} />
+            <div>
+              <h4>{data.address}</h4>
+              <h4>{data.region}</h4>
+            </div>
           </div>
+          <h4 className="text-xl text-right">${data.price} / hour</h4>
         </div>
       </div>
     </Link>
