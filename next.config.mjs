966b7b2201f6294846fefb5483190b7e39bbbaf5
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-13 15:04:42 -0500

    Uses new image configuration method replacing old depreciated method of domain name

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["tpasc.ca"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tpasc.ca",
+      },
+    ],
   },
 };
 
