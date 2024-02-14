48bd63b8e6cf74a2e77dabb15149f068fe72a4ac
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-14 18:32:18 -0500

    Edits use checkout hook function to retain booking data

--- hooks/useCheckout.ts
+++ hooks/useCheckout.ts
@@ -1,13 +1,17 @@
 import { create } from "zustand";
 
+import { Reservation } from "@prisma/client";
+
 interface CheckoutModalStore {
   isOpen: boolean;
+  reservationData: Reservation | undefined;
   onOpen: () => void;
   onClose: () => void;
 }
 
 const useCheckout = create<CheckoutModalStore>((set) => ({
   isOpen: false,
+  reservationData: undefined,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
