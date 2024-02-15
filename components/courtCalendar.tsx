c8948ca812df2d76a3544727d7f8f238405da94d
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-15 15:13:47 -0500

    Fixes typing of court calendar for events and handling of parameter types

--- components/courtCalendar.tsx
+++ components/courtCalendar.tsx
@@ -3,7 +3,7 @@
 import { useCallback, useState } from "react";
 
 import { Calendar, Views, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import moment, { Moment } from "moment";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { CourtWithReservationsAndSports } from "@/types";
@@ -11,8 +11,8 @@ import { CourtWithReservationsAndSports } from "@/types";
 const localizer = momentLocalizer(moment);
 
 type CourtReservationEvent = {
-  start: Date;
-  end: Date;
+  start: Moment;
+  end: Moment;
   title: string;
 };
 
@@ -26,14 +26,26 @@ interface CourtCalendarProps {
 }
 
 export const CourtCalendar = ({ data }: CourtCalendarProps) => {
-  const [events, setEvents] = useState(data.reservations);
+  const eventData = data.reservations.map((item) => {
+    return {
+      title: `Court Booked by ${item.bookingName}`,
+      start: moment(item.startTime),
+      end: moment(item.endTime),
+    };
+  });
+
+  const [events, setEvents] = useState<CourtReservationEvent[]>(eventData);
   const [reservation, setReservation] = useState<Array<CourtReservationEvent>>(
     []
   );
 
   const handleSelectSlot = useCallback(
     ({ start, end }: CourtEvent) => {
-      setReservation([{ start, end, title: "Court Reservation" }]);
+      let newStart = moment(start);
+      const newEnd = moment(end);
+      setReservation([
+        { start: newStart, end: newEnd, title: "Court Reservation" },
+      ]);
     },
     [setReservation]
   );
@@ -51,7 +63,7 @@ export const CourtCalendar = ({ data }: CourtCalendarProps) => {
         dayLayoutAlgorithm={"no-overlap"}
         defaultView={Views.WEEK}
         localizer={localizer}
-        events={reservation}
+        events={events}
         selectable
         onSelectEvent={handleSelectEvent}
         onSelectSlot={handleSelectSlot}
