4b0009a2739074ccab41af354767b4b6ffa58149
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-16 17:15:59 -0500

    Fixes navbar being covered by page content

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -4,7 +4,7 @@ import { Logo } from "./logo";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full shadow-sm flex items-center">
+    <nav className="fixed top-0 z-50 w-full shadow-sm flex items-center">
       <div className="flex flex-row w-full justify-between items-center bg-slate-700 py-4 px-8">
         <Logo />
         <div className="hidden lg:block text-3xl text-center text-white mr-20">
