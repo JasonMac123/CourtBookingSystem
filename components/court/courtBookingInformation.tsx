19a9a50f5ec144f744ed3b2413bf4ae3d85e3e4f
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-19 20:22:58 -0500

    Creates seperate component and converts time to 12 hour time with AM and PM

---
+++ components/court/courtBookingInformation.tsx
@@ -0,0 +1,36 @@
+import moment from "moment";
+
+import { CourtReservationEvent } from "@/types";
+
+interface CourtBookingInformationProps {
+  price: number;
+  reservation: CourtReservationEvent;
+}
+
+export const CourtBookingInformation = ({
+  price,
+  reservation,
+}: CourtBookingInformationProps) => {
+  const startPoint = reservation.start.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  const endPoint = reservation.end.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  const hours = reservation.end.getHours() - reservation.start.getHours();
+  const totalPrice = hours * price;
+
+  return (
+    <>
+      <div>
+        {startPoint} to {endPoint}
+      </div>
+      <div>${totalPrice} tax included</div>
+    </>
+  );
+};
