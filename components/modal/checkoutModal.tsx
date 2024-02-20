bf204faccb2414db61eff430d86d197d11b2ec59
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-19 21:36:52 -0500

    Adds checkout information about court and reservation time and event

--- components/modal/checkoutModal.tsx
+++ components/modal/checkoutModal.tsx
@@ -1,10 +1,33 @@
+"use client";
+
+import Image from "next/image";
+
 import { useCheckout } from "@/hooks/useCheckout";
+
 import Modal from "./modal";
+import { CourtWithReservationsAndSports } from "@/types";
 
-export const CheckoutModal = () => {
-  const { isOpen, onClose } = useCheckout();
+interface CheckoutModalProps {
+  data: CourtWithReservationsAndSports;
+}
 
-  const bodyContent = <div></div>;
+export const CheckoutModal = ({ data }: CheckoutModalProps) => {
+  const { isOpen, onClose, reservationData } = useCheckout();
+
+  const bodyContent = (
+    <div>
+      <div className="flex gap-4">
+        <div className="relative rounded-xl overflow-auto mx-auto w-60 h-40">
+          <Image
+            src={data.imageId}
+            alt="picture of court"
+            fill
+            className="object-fit"
+          />
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <Modal
