03d58fb89d7a9a2b6fa3ab2c7fde79cb85d3c2bc
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-19 21:49:22 -0500

    Adds second page of checkout modal to be  embedded stripe checkout page

--- components/modal/checkoutModal.tsx
+++ components/modal/checkoutModal.tsx
@@ -6,15 +6,38 @@ import { useCheckout } from "@/hooks/useCheckout";
 
 import Modal from "./modal";
 import { CourtWithReservationsAndSports } from "@/types";
+import { useMemo, useState } from "react";
 
 interface CheckoutModalProps {
   data: CourtWithReservationsAndSports;
 }
 
+enum STEPS {
+  INFO = 0,
+  CHECKOUT = 1,
+}
+
 export const CheckoutModal = ({ data }: CheckoutModalProps) => {
   const { isOpen, onClose, reservationData } = useCheckout();
+  const [step, setStep] = useState(STEPS.INFO);
 
-  const bodyContent = (
+  const buttonLabel = useMemo(() => {
+    if (step === STEPS.INFO) {
+      return "Confirm your purchase";
+    }
+    return "Checkout";
+  }, [step]);
+
+  const onSubmit = () => {
+    if (step !== STEPS.CHECKOUT) {
+      setStep((value) => value + 1);
+      return;
+    }
+
+    return;
+  };
+
+  let bodyContent = (
     <div>
       <div className="flex gap-4">
         <div className="relative rounded-xl overflow-auto mx-auto w-60 h-40">
@@ -29,11 +52,15 @@ export const CheckoutModal = ({ data }: CheckoutModalProps) => {
     </div>
   );
 
+  if (step === STEPS.CHECKOUT) {
+    bodyContent = <div id="checkout"></div>;
+  }
+
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
-      buttonTitle="Purchase"
+      buttonTitle={buttonLabel}
       body={bodyContent}
     />
   );
