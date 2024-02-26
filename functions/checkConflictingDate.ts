aa7bf432c750b07608a34c022adcedd138e5df9d
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-25 20:37:20 -0500

    Refactors function to take in two dates instead of large court reservation event object

--- functions/checkConflictingDate.ts
+++ functions/checkConflictingDate.ts
@@ -4,32 +4,24 @@ import { CourtReservationEvent } from "@/types";
 
 export const checkConflictingDate = (
   events: CourtReservationEvent[],
-  date: CourtReservationEvent
+  start: Date,
+  end: Date
 ): boolean => {
   for (const event of events) {
     // three scenarios if current date is overlapping
     // #1 current reservation time ends between the start and end of past reservation date
-    if (
-      moment(event.start).isBefore(date.end) &&
-      moment(event.end).isAfter(date.end)
-    ) {
+    if (moment(event.start).isBefore(end) && moment(event.end).isAfter(end)) {
       return false;
     }
 
     // #2 current reservation time starts between the start and end of past reservation date
-    if (
-      moment(event.start).isBefore(date.start) &&
-      moment(event.end).isAfter(date.end)
-    ) {
+    if (moment(event.start).isBefore(start) && moment(event.end).isAfter(end)) {
       return false;
     }
 
     // #3 current reservation time stars before the start of the past
     //    reservation date and ends after the past reservation date
-    if (
-      moment(event.start).isAfter(date.start) &&
-      moment(event.end).isBefore(date.end)
-    ) {
+    if (moment(event.start).isAfter(start) && moment(event.end).isBefore(end)) {
       return false;
     }
   }
